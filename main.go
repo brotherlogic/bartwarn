4a06a24c8985f9a1ef4b82da7cf37afd444ca4b4
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-05-21 17:59:28 -0700

    Auto PR: impl-issue-13 (#15)
    * feat: implement main entrypoint and tcp listener
    
    Closes #13
    
    * fix: implement graceful shutdown for grpc server
    
    ---------
    
    Co-authored-by: Brotherlogic Automation <brotherlogicautomation@gmail.com>

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/brotherlogic/bartwarn/api"
+	"github.com/brotherlogic/bartwarn/server"
+	"google.golang.org/grpc"
+)
+
+// Dummy implementations for unbuilt modules
+type dummyRouter struct{}
+func (r *dummyRouter) FindRoute(stationId string) error {
+	slog.Info("Dummy Router called", "stationId", stationId)
+	return nil
+}
+
+type dummyNotifier struct{}
+func (n *dummyNotifier) SendSMS(message string) error {
+	slog.Info("Dummy Notifier called", "message", message)
+	return nil
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "50051"
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		slog.Error("Failed to listen on TCP port", "error", err)
+		os.Exit(1)
+	}
+
+	grpcServer := grpc.NewServer()
+	
+	router := &dummyRouter{}
+	notifier := &dummyNotifier{}
+	
+	bartwarnServer := server.NewBartwarnServer(router, notifier)
+	api.RegisterLocationServiceServer(grpcServer, bartwarnServer)
+
+	// Start server in a goroutine so it doesn't block
+	go func() {
+		slog.Info("Starting gRPC Server", "port", port)
+		if err := grpcServer.Serve(listener); err != nil {
+			slog.Error("Failed to serve gRPC", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Wait for OS interrupt signals for graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	slog.Info("Shutting down gRPC server gracefully...")
+	grpcServer.GracefulStop()
+	slog.Info("Server stopped")
+}
